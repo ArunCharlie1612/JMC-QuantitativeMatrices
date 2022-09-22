59a3d6f7faebe454a8b45009263e293ba5d57ac4
ArunCharlie1612 <110811081+ArunCharlie1612@users.noreply.github.com>
2022-09-22 17:21:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,610 @@
+function showContent112(){
+    let text = document.getElementById("div112").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent113(){
+    let text = document.getElementById("div113").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent121(){
+    let text = document.getElementById("div121").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent122(){
+    let text = document.getElementById("div122").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent132(){
+    let text = document.getElementById("div132").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent134(){
+    let text = document.getElementById("div134").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent141(){
+    let text = document.getElementById("div141").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent142(){
+    let text = document.getElementById("div142").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent211(){
+    let text = document.getElementById("div211").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent212(){
+    let text = document.getElementById("div212").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent213(){
+    let text = document.getElementById("div213").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent222(){
+    let text = document.getElementById("div222").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent223(){
+    let text = document.getElementById("div223").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent232(){
+    let text = document.getElementById("div232").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent233(){
+    let text = document.getElementById("div233").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent241(){
+    let text = document.getElementById("div241").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent242(){
+    let text = document.getElementById("div242").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent243(){
+    let text = document.getElementById("div243").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent244(){
+    let text = document.getElementById("div244").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent245(){
+    let text = document.getElementById("div245").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent251(){
+    let text = document.getElementById("div251").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent252(){
+    let text = document.getElementById("div252").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent253(){
+    let text = document.getElementById("div253").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent255(){
+    let text = document.getElementById("div255").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent263(){
+    let text = document.getElementById("div263").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent311(){
+    let text = document.getElementById("div311").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent312(){
+    let text = document.getElementById("div312").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent313(){
+    let text = document.getElementById("div313").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent314(){
+    let text = document.getElementById("div314").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent321(){
+    let text = document.getElementById("div321").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent322(){
+    let text = document.getElementById("div322").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent323(){
+    let text = document.getElementById("div323").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent324(){
+    let text = document.getElementById("div324").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent332(){
+    let text = document.getElementById("div332").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent333(){
+    let text = document.getElementById("div333").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent334(){
+    let text = document.getElementById("div334").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent341(){
+    let text = document.getElementById("div341").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent342(){
+    let text = document.getElementById("div342").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent343(){
+    let text = document.getElementById("div343").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent344(){
+    let text = document.getElementById("div344").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent345(){
+    let text = document.getElementById("div345").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent346(){
+    let text = document.getElementById("div346").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent347(){
+    let text = document.getElementById("div347").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+
+function showContent348(){
+    let text = document.getElementById("div348").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent351(){
+    let text = document.getElementById("div351").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent352(){
+    let text = document.getElementById("div352").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent353(){
+    let text = document.getElementById("div353").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+function showContent362(){
+    let text = document.getElementById("div362").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent363(){
+    let text = document.getElementById("div363").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent364(){
+    let text = document.getElementById("div364").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent371(){
+    let text = document.getElementById("div371").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent372(){
+    let text = document.getElementById("div372").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent373(){
+    let text = document.getElementById("div373").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent413(){
+    let text = document.getElementById("div413").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent414(){
+    let text = document.getElementById("div414").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent423(){
+    let text = document.getElementById("div423").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent424(){
+    let text = document.getElementById("div424").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent425(){
+    let text = document.getElementById("div425").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent426(){
+    let text = document.getElementById("div426").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent432(){
+    let text = document.getElementById("div432").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent433(){
+    let text = document.getElementById("div433").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent434(){
+    let text = document.getElementById("div434").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent441(){
+    let text = document.getElementById("div441").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent511(){
+    let text = document.getElementById("div511").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent512(){
+    let text = document.getElementById("div512").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent513(){
+    let text = document.getElementById("div513").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent514(){
+    let text = document.getElementById("div514").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent515(){
+    let text = document.getElementById("div515").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent516(){
+    let text = document.getElementById("div516").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent521(){
+    let text = document.getElementById("div521").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent522(){
+    let text = document.getElementById("div522").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent523(){
+    let text = document.getElementById("div523").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent531(){
+    let text = document.getElementById("div531").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent533(){
+    let text = document.getElementById("div533").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent542(){
+    let text = document.getElementById("div542").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent543(){
+    let text = document.getElementById("div543").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent623(){
+    let text = document.getElementById("div623").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent632(){
+    let text = document.getElementById("div632").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent633(){
+    let text = document.getElementById("div633").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent634(){
+    let text = document.getElementById("div634").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+function showContent642(){
+    let text = document.getElementById("div642").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent653(){
+    let text = document.getElementById("div653").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent654(){
+    let text = document.getElementById("div654").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent711(){
+    let text = document.getElementById("div711").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent713(){
+    let text = document.getElementById("div713").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent714(){
+    let text = document.getElementById("div714").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent718(){
+    let text = document.getElementById("div718").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent719(){
+    let text = document.getElementById("div719").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent7110(){
+    let text = document.getElementById("div7110").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+
+function showContent7111(){
+    let text = document.getElementById("div7111").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent7112(){
+    let text = document.getElementById("div7112").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent7113(){
+    let text = document.getElementById("div7113").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+function showContent7114(){
+    let text = document.getElementById("div7114").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+function showContent7115(){
+    let text = document.getElementById("div7115").innerHTML;
+    document.getElementById("empty").innerHTML = text;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
